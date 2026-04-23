f215d8bef75fa3947758d40be30ef72aecce0d22
Eros Campos <eroscamposg@gmail.com>
2026-04-23 13:16:28 -0500

    Uploading images to supabse storage

--- src/services/apiCabins.js
+++ src/services/apiCabins.js
@@ -1,4 +1,4 @@
-import supabase from './supabase';
+import supabase, { supabaseUrl } from './supabase';
 
 export async function getCabins() {
   const { data: cabins, error } = await supabase.from('cabins').select('*');
@@ -23,14 +23,36 @@ export async function deleteCabin(id) {
 }
 
 export async function createCabin(newCabin) {
+  const imageName = `${Math.random()}-${newCabin.image.name}`.replaceAll(
+    '/',
+    ''
+  );
+
+  const imagePath = `${supabaseUrl}/storage/v1/object/public/cabin-images/${imageName}`;
+
+  // 1. Create cabin
   const { data, error } = await supabase
     .from('cabins')
-    .insert([newCabin])
+    .insert([{ ...newCabin, image: imagePath }])
     .select();
 
   if (error) {
     console.error(error);
     throw new Error('Cabins could not be created');
+  }
+
+  // 2. Upload images
+  const { error: storageError } = await supabase.storage
+    .from('cabin-images')
+    .upload(imageName, newCabin.image);
+
+  // 3. Delete the cabin if theres an error uploading the image
+  if (storageError) {
+    console.log(storageError);
+    await supabase.from('cabins').delete().eq('id', data.id);
+    throw new Error(
+      'Cabin image could not be uploaded and the cabin was not created'
+    );
   }
 
   return data;
