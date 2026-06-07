991229ae67512dabf096e6d6f7d2b99833d40ba0
Eros Campos <eroscamposg@gmail.com>
2026-06-07 15:34:13 -0500

    Adding protected route logic and logout functionality

--- src/features/authentication/useLogin.js
+++ src/features/authentication/useLogin.js
@@ -1,15 +1,18 @@
-import { useMutation } from '@tanstack/react-query';
+import { useMutation, useQueryClient } from '@tanstack/react-query';
 import { login as loginApi } from '../../services/apiAuth';
 import { useNavigate } from 'react-router-dom';
 import toast from 'react-hot-toast';
 
 export function useLogin() {
   const navigate = useNavigate();
+  const queryClient = useQueryClient();
   const { mutate: login, isPending: isLoading } = useMutation({
     mutationKey: ['login'],
     mutationFn: ({ email, password }) => loginApi({ email, password }),
-    onSuccess: () => {
-      navigate('/dashboard');
+    onSuccess: data => {
+      // This is to avoid an extra getCurrentUser api call when redirecting to a ProtectedRoute page
+      queryClient.setQueryData(['user'], data.user);
+      navigate('/dashboard', { replace: true });
     },
     onError: err => {
       toast.error(err.message);
---
+++ src/ui/ProtectedRoute.jsx
@@ -0,0 +1,38 @@
+import styled from 'styled-components';
+import useUser from '../features/authentication/useUser';
+import Spinner from './Spinner';
+import { useNavigate } from 'react-router-dom';
+import { useEffect } from 'react';
+
+const FullPage = styled.div`
+  height: 100vh;
+  background-color: var(--color-grey-500);
+  display: flex;
+  align-items: center;
+  justify-content: center;
+`;
+
+function ProtectedRoute({ children }) {
+  const navigate = useNavigate();
+
+  //1. Load authenticated user
+  const { isAuthenticated, isLoading } = useUser();
+
+  //3. If there is no authenticathed user, return to the /login
+  useEffect(() => {
+    if (!isAuthenticated && !isLoading) navigate('/login');
+  }, [isAuthenticated, isLoading, navigate]);
+
+  //2. While loading, show a spinner
+  if (isLoading)
+    return (
+      <FullPage>
+        <Spinner />
+      </FullPage>
+    );
+
+  //4. If there is a user, render the app
+  if (isAuthenticated) return children;
+}
+
+export default ProtectedRoute;
