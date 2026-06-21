57087edc58bc4d691fdd4732bc21a47b31e707d4
Eros Campos <eroscamposg@gmail.com>
2026-06-20 19:42:07 -0500

    Displaying dashboards's today activity

--- src/features/check-in-out/CheckoutButton.jsx
+++ src/features/check-in-out/CheckoutButton.jsx
@@ -1,8 +1,16 @@
-import Button from "../../ui/Button";
+import Button from '../../ui/Button';
+import { useCheckOut } from './useCheckOut';
 
 function CheckoutButton({ bookingId }) {
+  const { checkout, isCheckingOut } = useCheckOut();
+
   return (
-    <Button variation="primary" size="small">
+    <Button
+      variation="primary"
+      size="small"
+      disabled={isCheckingOut}
+      onClick={() => checkout(bookingId)}
+    >
       Check out
     </Button>
   );
